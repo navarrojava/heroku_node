6f49977f67096242a63fba5916d1be20cb2630a5
Navarro Ferreira <navarro.pod@gmail.com>
2016-07-03 17:53:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 //Lets require/import the HTTP module
 var http = require('http');
+var externalip =require('externalip');
+
+var publicIp= "";
+
+externalip(function (err, ip) {
+	publicIp = ip;
+  console.log(publicIp); // => 8.8.8.8
+});
 
 //Lets define a port we want to listen to
 //const PORT=80; 
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('Backend teste SelfSecurity, voce esta no path: ' + request.url);
+    response.end('Backend teste SelfSecurity, voce esta no path: ' + request.url + ' public ip: ' + publicIp);
 }
 
 //Create a server
@@ -16,3 +24,25 @@ var server = http.createServer(handleRequest);
 server.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in mode", this.address().port);
 });
+
+
+
+
+// //Lets require/import the HTTP module
+// var http = require('http');
+
+// //Lets define a port we want to listen to
+// //const PORT=80; 
+
+// //We need a function which handles requests and send response
+// function handleRequest(request, response){
+//     response.end('Backend teste SelfSecurity, voce esta no path: ' + request.url);
+// }
+
+// //Create a server
+// var server = http.createServer(handleRequest);
+
+// //Lets start our server
+// server.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in mode", this.address().port);
+// });
